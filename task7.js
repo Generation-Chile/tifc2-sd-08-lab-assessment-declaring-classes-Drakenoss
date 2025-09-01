b9bfea1a0a839b78e0d3141114cd31e57daf1672
Sebastrian <sebastian.angel@genstudents.org>
2025-09-01 00:33:23 +0000

    tarea

--- task7.js
+++ task7.js
@@ -1,38 +1,40 @@
 export class Player {
-    constructor(name,level) {
-     this.name = name;
-     this.level = level;
-     this.experience = 0;
-     this.exptolevel = 100;
-     this.inventory = new Map();
-      }
-    }
-  info() {
-    console.log(`${this.name} has reached Level ${this.level}!`);
-    
-  }
-  levelUp() {
-    this.level += 1;
-    this.experience = 0;
-    console.log(`${this.name} has leveled up to Level ${this.level}!`);
-  }
+    constructor(name, level) {
+        this.name = name;
+        this.level = level;
+        this.experience = 0;
+        this.exptolevel = 100;
+        this.inventory = new Map();
+    }
 
-  addItem(itemName, quantity = 1) {
-    const currentQty = this.inventory.get(itemName) || 0;
-    this.inventory.set(itemName, currentQty + quantity);
-  }
+    info() {
+        console.log(`${this.name} has reached Level ${this.level}!`);
+    }
 
-  removeItem(itemName, quantity = 1) {
-    const currentQty = this.inventory.get(itemName) || 0;
-    if (currentQty <= quantity) {
-      this.inventory.delete(itemName);
-    } else {
-      this.inventory.set(itemName, currentQty - quantity);
-    }
-  }
-  listInventory() {
-    console.log(`${this.name}'s Inventory:`);
-    for (const [item, qty] of this.inventory.entries()) {
-      console.log(`${item}: ${qty}`);
-    }
-  }
+    levelUp() {
+        this.level += 1;
+        this.experience = 0;
+        console.log(`${this.name} has leveled up to Level ${this.level}!`);
+    }
+
+    addItem(itemName, quantity = 1) {
+        const currentQty = this.inventory.get(itemName) || 0;
+        this.inventory.set(itemName, currentQty + quantity);
+    }
+
+    removeItem(itemName, quantity = 1) {
+        const currentQty = this.inventory.get(itemName) || 0;
+        if (currentQty <= quantity) {
+            this.inventory.delete(itemName);
+        } else {
+            this.inventory.set(itemName, currentQty - quantity);
+        }
+    }
+
+    listInventory() {
+        console.log(`${this.name}'s Inventory:`);
+        for (const [item, qty] of this.inventory.entries()) {
+            console.log(`${item}: ${qty}`);
+        }
+    }
+}
